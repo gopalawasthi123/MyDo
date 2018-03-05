b600183db157184a400fb347235f6c52fd95060f
gopalawasthi123 <gopalawasthi383@gmail.com>
2018-03-06 01:25:19 +0530

    adding alarms and notification through broadcast recceiver

---
+++ app/src/main/java/com/example/gopalawasthi/mydo/MyReceiver.java
@@ -0,0 +1,35 @@
+package com.example.gopalawasthi.mydo;
+
+import android.app.Notification;
+import android.app.NotificationManager;
+import android.app.PendingIntent;
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.graphics.Color;
+import android.os.Build;
+import android.support.v4.app.NotificationCompat;
+
+public class MyReceiver extends BroadcastReceiver {
+
+    @Override
+    public void onReceive(Context context, Intent intent) {
+
+        NotificationManager notificationManager = (NotificationManager) context.getSystemService(Context.NOTIFICATION_SERVICE);
+        NotificationCompat.Builder  builder = new NotificationCompat.Builder(context,"myapp");
+        builder.setContentTitle("MyDo");
+        builder.setContentText("your do is ready");
+        builder.setVibrate(new long[] {800,800,800,800});
+        builder.setLights(Color.RED,3000,2000);
+        builder.setSmallIcon(R.mipmap.ic_launcher);
+        Intent intent1 = new Intent(context,MainActivity.class);
+
+        PendingIntent pendingIntent = PendingIntent.getActivity(context,1,intent1,0);
+        builder.setContentIntent(pendingIntent);
+        Notification notification =  builder.build();
+
+        notificationManager.notify(1,notification);
+
+    }
+
+}
